1f48d7bfd1fc69a79c57c5d0feb62fcef1b818cb
Vadim <explosive.va@gmail.com>
2023-01-22 18:35:44 +0500

    min

--- lua/engineer/plugins/lsp/null-ls.lua
+++ lua/engineer/plugins/lsp/null-ls.lua
@@ -18,7 +18,8 @@ null_ls.setup({
 		--  to disable file types use
 		--  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
 		formatting.stylua, -- lua formatter
-		formatting.autopep8, --python formatter
+		formatting.autopep8, -- python formatter
+		formatting.autoflake, -- python formatter
 		diagnostics.eslint_d.with({ -- js/ts linter
 			-- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
 			condition = function(utils)
